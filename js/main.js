c78bbd0d29f59df4cc3231f3345a57e700dad297
MarioLizarraga <mario.lizarragat@gmail.com>
2024-08-22 10:31:29 -0700

    Initial commit with NASA WorldWind integration

--- js/main.js
+++ js/main.js
@@ -1,18 +1,16 @@
-// Create the WorldWindow (globe)
-var wwd = new WorldWind.WorldWindow("globe-container");
+console.log("JavaScript file is loaded");
 
-// Add a base layer of imagery
-var layers = [
-    new WorldWind.BMNGLayer(), // Blue Marble imagery layer
-    new WorldWind.BMNGLandsatLayer(), // Landsat imagery layer
-    new WorldWind.CoordinatesDisplayLayer(wwd), // Displays the coordinates of the mouse
-    new WorldWind.ViewControlsLayer(wwd) // Add view controls for zoom and pan
-];
+// Create a WorldWindow for the canvas.
+var wwd = new WorldWind.WorldWindow("canvasOne");
 
-// Add layers to the WorldWindow
-for (var i = 0; i < layers.length; i++) {
-    wwd.addLayer(layers[i]);
-}
+// Add some image layers to the WorldWindow's globe.
+wwd.addLayer(new WorldWind.BMNGOneImageLayer());
+wwd.addLayer(new WorldWind.BMNGLandsatLayer());
+
+// Add a compass, a coordinates display and some view controls to the WorldWindow.
+wwd.addLayer(new WorldWind.CompassLayer());
+wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
+wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
 
 // Redraw the WorldWindow (necessary to display layers)
 wwd.redraw();
